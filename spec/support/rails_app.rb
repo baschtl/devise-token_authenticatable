f0037c95dd5e4829db6deed51ecfd631e90f5faf
Sebastian Oelke <soelke@babbel.com>
2014-08-18 17:22:10 +0200

    Set a config option that caused deprecation warnings in specs with the current rails and i18n stack.

--- spec/support/rails_app.rb
+++ spec/support/rails_app.rb
@@ -11,6 +11,7 @@ module Devise
       config.active_support.deprecation         = :log
       config.action_mailer.default_url_options  = { :host => "localhost:3000" }
       config.action_mailer.delivery_method      = :test
+      config.i18n.enforce_available_locales     = false
       config.eager_load                         = false
     end
   end
