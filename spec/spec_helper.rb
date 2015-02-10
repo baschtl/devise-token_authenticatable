bd1aac48fe49c45819d164a22a4d42be5eaaaf65
Sebastian Oelke <soelke@babbel.com>
2015-02-10 16:34:10 +0100

    Cleaned up spec helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,16 +24,8 @@ ActiveRecord::Migration.verbose = false
 # RSpec configuration
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  config.use_transactional_fixtures                       = true
-  config.run_all_when_everything_filtered                 = true
-
-  config.filter_run :focus
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  #config.order = 'random'
+  config.use_transactional_fixtures       = true
+  config.run_all_when_everything_filtered = true
 
   config.include FactoryGirl::Syntax::Methods
 
